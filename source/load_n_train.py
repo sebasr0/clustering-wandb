da847215b7c1d91fba021bbe7854988d919a3465
sebasr0 <sebastian.ramirezesc@gmail.com>
2024-02-23 14:25:17 -0500

    test 3: log and plot

--- source/load_n_train.py
+++ source/load_n_train.py
@@ -4,7 +4,7 @@ import pandas as pd
 import wandb
 import argparse
 from sklearn.cluster import KMeans
-from sklearn import datasets, cluster
+from sklearn import datasets, metrics
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--IdExecution', type=str, help='ID of the execution')
@@ -28,6 +28,15 @@ def load_and_train():
         # Train model
         kmeans = KMeans(n_clusters=4, random_state=1)
         cluster_labels = kmeans.fit_predict(X)
+        
+        # Log the model parameters
+        wandb.config.update(kmeans.get_params())
+        
+        # plots
+        #wandb.sklearn.plot_elbow_curve(kmeans, X) #elow plot
+        #wandb.sklearn.plot_silhouette(kmeans, X, labels) #Silhouette plot
+        wandb.sklearn.plot_clusterer(kmeans, X, cluster_labels, labels, 'KMeans')
+        
         
 #execute second exec
 load_and_train()
